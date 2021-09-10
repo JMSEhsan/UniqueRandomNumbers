63e2a3ed01c24d97c0cd7bb07f328abf05f30d37
JMSEhsan <JMSEhsan@gmail.com>
2021-09-09 20:17:01 -0600

    Commit

--- Program.cs
+++ Program.cs
@@ -8,24 +8,24 @@ namespace UniqueRandomNumbers
     {
         static void Main(string[] args)
         {
-            Console.Write("Unique Random Generator <1-10000>:");
+            Console.WriteLine("Unique Random Generator <1 - 10000>:");
             Random rand = new Random();
             List<int> numbers = new List<int>();
-            for (int i = 0; i < 100; ++i)
-                numbers.Add(rand.Next(1, 101));
-                
+            for (int i = 0; i < 20; i++)
+                numbers.Add(rand.Next(1, 21));
+                Console.WriteLine("***"+ numbers.Count);
                 Display(numbers);
                 //Console.Read();
                 //Console.Write(numbers);
         }
                  
-        static void Display(List<int> list)             //Display method shows 50 number in each raw
-        {
-            for(int i = 0; i < list.Count; ++i)
-                if (i % 50 == 0)
-                   Console.WriteLine();
+        static void Display(List<int> list)             //Display method shows 50 number in each row
+        {   
+            for(int i = 0; i < list.Count; i++)
+                if(i % 10 == 0)
+                   Console.WriteLine(list[i] + ", ");
                 else
-                   Console.Write(list[i] + " ");
+                   Console.Write(list[i] + ", ");
         }
     }
 }
